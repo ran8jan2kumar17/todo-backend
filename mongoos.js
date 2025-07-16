f439debc820846a5773d6dd6af044fc0c276f112
Ranjan Kumar <ranjankumar69320@gmail.com>
2025-07-16 13:14:23 +0530

    Fix MongoDB SSL config for Render

--- mongoos.js
+++ mongoos.js
@@ -1,10 +1,18 @@
 import mongoose from 'mongoose';
-mongoose.connect(process.env.MONGO_URL)
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  ssl: true, // Important for MongoDB Atlas
+})
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.error("MongoDB connection error:", err));
 
 const sh = mongoose.Schema({
   ur: { type: String, required: true },
   pass: { type: Number, required: true },
   todos: [{ todo: String, isDone: Boolean }],
 });
+
 const sh1 = mongoose.model('todouser', sh);
 export default sh1;
